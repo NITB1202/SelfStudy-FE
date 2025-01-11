acf8c2ce185ef206d3f943023a683a087bd327a2
Test_repo <22520992@gm.uit.edu.vn>
2025-01-11 10:13:40 +0700

    Fix UI Intro page

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,6 @@ import { router } from "expo-router";
 export default function Index() {
   const { fontsLoaded } = useCustomFonts();
 
-  // Kiểm tra xem font đã tải xong chưa, nếu chưa thì không render nội dung
   if (!fontsLoaded) {
     return null;
   }
@@ -15,39 +14,27 @@ export default function Index() {
     <View style={styles.container}>
       <Text style={styles.title}>StudyPal</Text>
 
-      {/* Hình ảnh minh họa */}
       <Image
         source={require("../assets/images/IndexPicture.png")}
         style={styles.image}
         resizeMode="contain"
       />
 
-      {/* Phần mô tả */}
-      <Text style={styles.subtitle}>Welcome to StudyPal</Text>
-      <Text style={styles.description}>
-        StudyPal is a self-learning app that helps users organize their study
-        routine, track progress, and stay motivated to achieve their goals.
+      <View style={styles.textContainer}>
+        <Text style={styles.subtitle}>Welcome to StudyPal</Text>
+        <Text style={styles.description}>
+          StudyPal is a self-learning app that helps users organize their study
+          routine, track progress, and stay motivated to achieve their goals.
       </Text>
 
-      {/* Các nút */}
-      <View style={styles.buttonContainer}>
-        <CustomButton
-          title="Get Started"
-          // style={styles.buttonContainer}
-          // textStyle={styles.loginText}
-          onPress={() => {
-            router.push("/Intro/Login");
-          }}
-        />
-        {/* <CustomButton
-          title="Register"
-          style={styles.registerButton}
-          textStyle={styles.registerText}
-          onPress={() => {
-            router.push("/Intro/Register");
-          }}
-        /> */}
       </View>
+
+      <CustomButton
+        title="Get Started"
+        onPress={() => {
+          router.push("/Intro/Login");
+        }}
+      />
     </View>
   );
 }
@@ -55,60 +42,38 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    paddingVertical: 20, // Padding trên và dưới là 20
-    paddingHorizontal: 40, // Padding trái và phải là 40
     backgroundColor: "#FFFFFF",
+    gap: 40,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
   },
   title: {
     fontFamily: "PlusJakartaSans_700Bold",
     fontSize: 36,
     color: "#7AB2D3",
-    marginBottom: 40,
   },
   image: {
     width: 332,
-    height: 371,
-    marginBottom: 40,
+    height: 350,
   },
   subtitle: {
     fontFamily: "PlusJakartaSans_700Bold",
     fontSize: 20,
     color: Colors.black,
-    marginBottom: 10,
   },
   description: {
     fontFamily: "Roboto_400Regular",
     fontSize: 15,
     color: Colors.black,
-    textAlign: "center",
-    marginBottom: 60,
+    textAlign: "justify",
   },
-  buttonContainer: {
-    width: "100%",
-    justifyContent: "space-between",
+  textContainer:{
+    display: "flex",
+    justifyContent: "center",
     alignItems: "center",
-  },
-  loginButton: {
-    backgroundColor: "#7AB2D3", // Màu xanh nhạt
-    width: "85%",
-    paddingVertical: 10,
-    marginBottom: 15,
-  },
-  registerButton: {
-    backgroundColor: "#C0C0C0", // Màu xám
-    width: "85%",
-    paddingVertical: 10,
-  },
-  loginText: {
-    color: Colors.white, // Màu chữ trắng
-    fontFamily: "Poppins_700Bold",
-    fontSize: 16,
-  },
-  registerText: {
-    color: Colors.black, // Màu chữ đen
-    fontFamily: "Poppins_700Bold",
-    fontSize: 16,
-  },
+    gap: 10,
+  }
 });
