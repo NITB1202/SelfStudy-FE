592fda84b04f778af82ae6020cffa6f34c1112c8
Test_repo <22520992@gm.uit.edu.vn>
2025-01-14 17:19:45 +0700

    Complete send verification code page

--- api/authApi.ts
+++ api/authApi.ts
@@ -4,6 +4,10 @@ const authApi = {
     login: (email: string, password: string) => {
         return axiosInstance.post("/auth/login", {email, password});
     },
+    sendCode: (email: string) => {
+        const url = "/auth/mail?email="+email;
+        return axiosInstance.get(url);
+    }
 }
 
 export default authApi;
